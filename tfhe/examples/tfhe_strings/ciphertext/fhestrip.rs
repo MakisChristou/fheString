d299d8a250198eb2515ef92b199ce7d41a01648f
MakisChristou <makis_christou@protonmail.com>
2023-11-12 11:00:32 +0200

    Add custom return type for strip functions, FheStrip

---
+++ tfhe/examples/tfhe_strings/ciphertext/fhestrip.rs
@@ -0,0 +1,29 @@
+use crate::client_key::MyClientKey;
+use crate::FheAsciiChar;
+use crate::FheString;
+
+pub struct FheStrip {
+    pub string: FheString,
+    pub pattern_found: FheAsciiChar,
+}
+
+impl FheStrip {
+    pub fn new(string: FheString, pattern_found: FheAsciiChar) -> Self {
+        FheStrip {
+            string,
+            pattern_found,
+        }
+    }
+
+    // Equivalent to running collect() on the iterator
+    pub fn decrypt(
+        fhe_strip: FheStrip,
+        my_client_key: &MyClientKey,
+        padding: usize,
+    ) -> (String, u8) {
+        let decrypted_string = my_client_key.decrypt(fhe_strip.string, padding);
+        let decrypted_flag = my_client_key.decrypt_char(&fhe_strip.pattern_found);
+
+        (decrypted_string, decrypted_flag)
+    }
+}
