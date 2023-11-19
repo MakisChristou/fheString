baf155c76fb874340307fcfff5672c66ab3b29f3
Makis Christou <michalis.christou@protonmail.com>
2023-11-19 20:01:11 +0100

    Refactor: Move trim functionality in its own module

--- tfhe/examples/tfhe_strings/server_key/mod.rs
+++ tfhe/examples/tfhe_strings/server_key/mod.rs
@@ -8,6 +8,7 @@ use crate::{MAX_FIND_LENGTH, MAX_REPETITIONS};
 use serde::{Deserialize, Serialize};
 
 pub mod split;
+pub mod trim;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MyServerKey {
@@ -191,65 +192,6 @@ impl MyServerKey {
         }
 
         result
-    }
-
-    pub fn trim_end(&self, string: &FheString, public_parameters: &PublicParameters) -> FheString {
-        let zero = FheAsciiChar::encrypt_trivial(0u8, public_parameters);
-        let one = FheAsciiChar::encrypt_trivial(1u8, public_parameters);
-
-        let mut stop_trim_flag = zero.clone();
-        let mut result = vec![zero.clone(); string.bytes.len()];
-
-        // Replace whitespace with \0 starting from the end
-        for i in (0..string.bytes.len()).rev() {
-            let is_not_zero = string.bytes[i].ne(&self.key, &zero);
-
-            let is_not_whitespace = string.bytes[i]
-                .is_whitespace(&self.key, public_parameters)
-                .flip(&self.key, public_parameters);
-            stop_trim_flag = stop_trim_flag.bitor(
-                &self.key,
-                &is_not_whitespace.bitand(&self.key, &is_not_zero),
-            );
-            result[i] = stop_trim_flag.if_then_else(&self.key, &string.bytes[i], &zero);
-        }
-
-        FheString::from_vec(result, public_parameters)
-    }
-
-    pub fn trim_start(
-        &self,
-        string: &FheString,
-        public_parameters: &PublicParameters,
-    ) -> FheString {
-        let zero = FheAsciiChar::encrypt_trivial(0u8, public_parameters);
-
-        let mut stop_trim_flag = zero.clone();
-        let mut result = vec![zero.clone(); string.bytes.len()];
-
-        // Replace whitespace with \0 starting from the start
-        for (i, result_char) in result.iter_mut().enumerate().take(string.bytes.len()) {
-            let is_not_zero = string.bytes[i].ne(&self.key, &zero);
-            let is_not_whitespace = string.bytes[i]
-                .is_whitespace(&self.key, public_parameters)
-                .flip(&self.key, public_parameters);
-
-            stop_trim_flag = stop_trim_flag.bitor(
-                &self.key,
-                &is_not_whitespace.bitand(&self.key, &is_not_zero),
-            );
-            *result_char = stop_trim_flag.if_then_else(&self.key, &string.bytes[i], &zero)
-        }
-
-        FheString::from_vec(
-            utils::bubble_zeroes_left(result, &self.key, public_parameters),
-            public_parameters,
-        )
-    }
-
-    pub fn trim(&self, string: &FheString, public_parameters: &PublicParameters) -> FheString {
-        let result = self.trim_end(string, public_parameters);
-        self.trim_start(&result, public_parameters)
     }
 
     pub fn repeat_clear(
